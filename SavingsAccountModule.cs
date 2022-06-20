a72715ca073e82d5a9f47a3bc74fc46008a02537
Dawoods7 <dawoodshees@gmail.com>
2022-06-20 03:32:35 +0300

    Program implented according to the outline

--- SavingsAccountModule.cs
+++ SavingsAccountModule.cs
@@ -3,7 +3,7 @@ namespace Namespace {
     using Account = AccountModule.Account;
     
     using InvalidValue = AccountModule.InvalidValue;
-    //FINISHED
+    //FINISHED 
     public static class Module {
         
 
